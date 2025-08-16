70db0fb95c4606f53b7c662f2fc670d761e15f4a
VArch <farrelnikoson@gmail.com>
2025-08-17 00:05:58 +0700

    Graphics pipeline: shaders

--- base/main.cpp
+++ base/main.cpp
@@ -52,6 +52,23 @@ void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT
     }
 }
 
+static std::vector<char> readFile(const std::string& filename) {
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+    
+    if (!file.is_open()) {
+	throw std::runtime_error("failed to open file!");
+    }
+
+    size_t fileSize = (size_t) file.tellg();
+    std::vector<char> buffer(fileSize);
+    
+    file.seekg(0);
+    file.read(buffer.data(), fileSize);
+
+    file.close();
+    return buffer;
+}
+
 class HelloTriangleApplication {
     const uint32_t WIDTH = 800;
     const uint32_t HEIGHT = 600;
@@ -123,7 +140,10 @@ private:
         glfwDestroyWindow(window);
         glfwTerminate();
     }
-    void createGraphicsPipeline() {}
+    void createGraphicsPipeline() {
+	auto vertShaderCode = readFile("shaders/vert.spv");
+	auto fragShaderCode = readFile("shaders/frag.spv");
+    }
     void createImageViews() {
         swapChainImageViews.resize(swapChainImages.size());
 
